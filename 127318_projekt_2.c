9a6dccceda4f19869bab6b7ad85ec6189b6cca78
zimka1 <zimkail2015@mail.ru>
2023-11-13 19:33:13 +0100

    Структура ID
    Не знаю зачем, но её нужно было добавить

--- 127318_projekt_2.c
+++ 127318_projekt_2.c
@@ -3,15 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Neviem na čo, ale vytvárame túto štruktúru
+typedef struct ID{
+    char oznacenie;
+    int cislovanie;
+    char druh;
+} ID;
+
+
 typedef struct Zaznam {
-    char id[6];
+    ID id[6];
     char poz[17];
     char typ[3];
     double hod;
     char cas[5];
     int datum;
     char datumCas[20];
-    struct Zaznam *dalsi; // Ukazovateľ na ďalší záznam
+    struct Zaznam *dalsi;
 } Zaznam;
 
 void uvolnit_zaznamy(Zaznam **zaz) {
@@ -38,11 +46,19 @@ void nacitanie(FILE **dataloger, Zaznam **zaz, int *pocet_zaznamov) {
 
     while (!feof(*dataloger)) {
         akt = (Zaznam *)malloc(sizeof(Zaznam));
-
+        char ID_norm[6];
         if (fscanf(*dataloger, "%5s %16s %2s %lf %4s %d",
-                   akt->id, akt->poz, akt->typ,
+                   ID_norm, akt->poz, akt->typ,
                    &akt->hod, akt->cas, &akt->datum) == 6) {
             sprintf(akt->datumCas, "%d%s", akt->datum, akt->cas);
+            akt->id->oznacenie = ID_norm[0];
+            akt->id->druh = ID_norm[4];
+            char middleThree[3];
+            for (int i = 1; i < 4; i++) {
+                middleThree[i - 1] = ID_norm[i];
+            }
+            akt->id->cislovanie = atoi(middleThree);
+
             akt->dalsi = NULL;
             if (posledny != NULL) {
                 posledny->dalsi = akt;
@@ -65,7 +81,7 @@ void vypis_zaznamov(Zaznam *zaz) {
     int index = 1;
     while (akt != NULL) {
         printf("%d:\n", index);
-        printf("ID: %s\t%s\t%2lf\n", akt->id, akt->typ, akt->hod);
+        printf("ID: %c%d%c\t%s\t%2lf\n", akt->id->oznacenie, akt->id->cislovanie, akt->id->druh, akt->typ, akt->hod);
         printf("Poz: %.8s\t%.8s\n", akt->poz, akt->poz + 8);
         printf("DaC: %d\t%s\n", akt->datum, akt->cas);
         printf("%s\n", akt->datumCas);
@@ -83,7 +99,19 @@ void novy_zaznam(Zaznam **zaz, int *pocet_zaznamov) {
     int index = 1;
     int prov = 0;
     Zaznam *novyZaznam = (Zaznam *)malloc(sizeof(Zaznam));
-    scanf("%5s %16s %2s %lf %4s %d", novyZaznam->id, novyZaznam->poz, novyZaznam->typ, &novyZaznam->hod, novyZaznam->cas, &novyZaznam->datum);
+
+    char ID_norm[6];
+
+    scanf("%5s %16s %2s %lf %4s %d", ID_norm, novyZaznam->poz, novyZaznam->typ, &novyZaznam->hod, novyZaznam->cas, &novyZaznam->datum);
+    
+    novyZaznam->id->oznacenie = ID_norm[0];
+    novyZaznam->id->druh = ID_norm[4];
+    char middleThree[3];
+    for (int i = 1; i < 4; i++) {
+        middleThree[i - 1] = ID_norm[i];
+    }
+    novyZaznam->id->cislovanie = atoi(middleThree);
+
     novyZaznam->dalsi = NULL;
     if (miesto == 1) {
         novyZaznam->dalsi = *zaz;
@@ -107,12 +135,22 @@ void novy_zaznam(Zaznam **zaz, int *pocet_zaznamov) {
 
 void vymaz_zaznamov(Zaznam **zaz, int *pocet_zaznamov) 
 {
-    char id[6];
-    scanf("%5s", id);
+    char ID_norm[6];
+    scanf("%5s", ID_norm);
+
+    char oznacenie_vym = ID_norm[0];
+    char druh_vym = ID_norm[4];
+    char middleThree[3];
+    for (int i = 1; i < 4; i++) {
+        middleThree[i - 1] = ID_norm[i];
+    }
+    int cislovanie_vym = atoi(middleThree);
+    printf("%d\n", cislovanie_vym);
     Zaznam *akt = *zaz;
     Zaznam *pred = NULL;
     while (akt!= NULL) {
-        if (strcmp(akt->id, id) == 0) {
+        if (akt->id->cislovanie == cislovanie_vym && akt->id->oznacenie == oznacenie_vym && akt->id->druh == druh_vym) {
+
             if (pred == NULL) {
                 *zaz = akt->dalsi;
             }
@@ -125,7 +163,7 @@ void vymaz_zaznamov(Zaznam **zaz, int *pocet_zaznamov)
             free(pam);
             (*pocet_zaznamov)--;
 
-            printf("Zaznam pre ID: %s bol vymazany.\n", id);
+            printf("Zaznam pre ID: %c%d%c bol vymazany.\n", oznacenie_vym, cislovanie_vym, druh_vym);
             continue;
         }
         pred = akt;
@@ -247,12 +285,10 @@ void zmena_zaznamov(Zaznam **zaz, int pocet_zaznamov) {
         temp = NULL;
         temp = zap2;
         temp->dalsi = zap1;
-        printf("%s\n", temp->dalsi->poz);
         Zaznam *akt = *zaz;
         for (int i = 1; i < c1 - 1 && akt!= NULL; i++){
             akt = akt->dalsi;
         }
-        printf("%s\n", akt->poz);
         if (akt == NULL) {
             return;
         }
@@ -260,10 +296,6 @@ void zmena_zaznamov(Zaznam **zaz, int pocet_zaznamov) {
         return;
     }
 
- 
-
-    printf("%s\n", zap1->poz);
-    printf("%s\n", zap2->poz);
 
     if (zap1 == NULL || zap2 == NULL) {
         return; // Один из узлов не найден
